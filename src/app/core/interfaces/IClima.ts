7a8e6b98aa32dd43113f63eb647d510b1747677a
Deyson Venté <deiandresvent@hotmail.com>
2024-12-25 23:19:56 -0500

    modelo de datos

---
+++ src/app/core/interfaces/IClima.ts
@@ -0,0 +1,10 @@
+export interface IClima {
+  temperatura: number;
+  humedad: string;
+  clima: string;
+  descripcion?: string;
+  feels_like?: number;
+  temp_min?: number;
+  temp_max?: number;
+  pressure?: number;
+}
