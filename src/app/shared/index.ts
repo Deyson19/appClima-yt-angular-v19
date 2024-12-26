d31a75da48ebe38ba168cdc94d5c1e7660a66be7
Deyson Venté <deiandresvent@hotmail.com>
2024-12-25 23:21:23 -0500

    archivo de barril para el shared

---
+++ src/app/shared/index.ts
@@ -0,0 +1,4 @@
+export { NavbarComponent } from './navbar/navbar.component';
+
+export { ErrorComponent } from './error/error.component';
+export { SearchComponent } from './search/search.component';
