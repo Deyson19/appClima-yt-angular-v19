29d3b5d46472f8a5ce85e623cb16b91c350c4864
Deyson Venté <deiandresvent@hotmail.com>
2024-12-25 23:22:09 -0500

    Update app configuration with new providers
    Refactor app.config.ts to use experimental zoneless change detection, add providers for animations, Toastr notifications, and HttpClient.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideToastr } from 'ngx-toastr';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideAnimations(),
+    provideRouter(routes),
+    provideToastr({
+      progressBar: true,
+    }),
+    provideHttpClient(),
+  ],
 };
